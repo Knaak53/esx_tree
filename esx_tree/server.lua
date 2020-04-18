07ecf875e8d940bc36b139a0ff834b1646e21a0f
arnevdaele <arne.van.daele@icloud.com>
2020-04-18 12:04:04 +0200

    Implement ESX Weight system instead of limits.

--- esx_tree/server.lua
+++ esx_tree/server.lua
@@ -9,6 +9,10 @@ AddEventHandler('Tree:GetWood', function()
     local xPlayer = ESX.GetPlayerFromId(source)
     local amount = math.random(2, 4)
 
-    xPlayer.addInventoryItem("wood", amount)
-    TriggerClientEvent('esx:showNotification', source, '你獲得了 ~g~' .. amount .. ' ~w~片木板')
+    if xPlayer.canCarryItem("wood", amount) then
+        xPlayer.addInventoryItem("wood", amount)
+        TriggerClientEvent('esx:showNotification', source, 'You received ~g~' .. amount .. ' ~w~ wood')
+    else
+        TriggerClientEvent('esx:showNotification', source, 'You cant carry this item')
+    end
 end)
